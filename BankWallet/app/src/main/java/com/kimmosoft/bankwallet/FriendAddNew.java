f7326e9061ae9e7a56acccd521cc1f091ea87136
Tuomas Aho <tuomasaho91@gmail.com>
2016-01-25 12:26:54 +0200

    Back button no functional on activity_firend_add_new

--- BankWallet/app/src/main/java/com/kimmosoft/bankwallet/FriendAddNew.java
+++ BankWallet/app/src/main/java/com/kimmosoft/bankwallet/FriendAddNew.java
@@ -1,7 +1,11 @@
 package com.kimmosoft.bankwallet;
 
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class FriendAddNew extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class FriendAddNew extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_add_new);
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null){
+            //actionBar.setTitle(realmHelper.getFriend(friendtId).getName() + "'s accounts");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. Use NavUtils to allow users
+            // to navigate up one level in the application structure. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+           NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
